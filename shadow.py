04406c97c26eb1057e470de95275526b5062694b
Tim Konick <konick781@gmail.com>
2014-02-09 14:35:09 -0500

    docstrings

--- shadow.py
+++ shadow.py
@@ -9,7 +9,11 @@ from connection import __NetLinkConn
 
 
 class Profile(__NetLinkConn):
-
+    '''
+    Profile object: builds a <Profile> object from the "pid" provided on
+    initialization.
+    '''
+    
     def __init__(self, pid):
         super(Profile, self).__init__()
         self.pid = pid
@@ -20,6 +24,9 @@ class Profile(__NetLinkConn):
 
     @property
     def is_alive(self):
+        '''
+        Class property: returns <type 'bool'> response of processes state.
+        '''
         all_processes = filter(lambda i: i.isdigit(), os.listdir('/proc'))
         if self.pid in map(int, all_processes):
             return True
@@ -27,6 +34,9 @@ class Profile(__NetLinkConn):
 
     @property
     def parent(self):
+        '''
+        Class property: returns <type 'int'> of the profiled pid's parent.
+        '''
         try:
             with open('/proc/%s/stat' % self.pid) as f:
                 pid_stats = f.read()
@@ -38,6 +48,9 @@ class Profile(__NetLinkConn):
 
     @property
     def name(self):
+        '''
+        Class property: returns <type 'str'> of profiled pid's name.
+        '''
         try:
             with open('/proc/%s/comm' % self.pid) as f:
                 pid_name = f.read()
@@ -48,6 +61,10 @@ class Profile(__NetLinkConn):
 
     @property
     def fds(self):
+        '''
+        Class property: returns <type 'int'> of open file-descriptors of 
+        profiled pid.
+        '''
         try:
             fds = os.listdir('/proc/%s/fd' % self.pid)
         except OSError:
@@ -56,23 +73,42 @@ class Profile(__NetLinkConn):
 
     @property
     def gid(self):
+        '''
+        Class property: returns <type 'str'> or <type 'NoneType'> of profiled 
+        pids group id.
+        '''
         attrs = self.__pid_attrs()
         return attrs.get('gids')
 
     @property
     def threads(self):
+        '''
+        Class property: returns <type 'str'> or <type 'NoneType'> of profiled 
+        pids thread count.
+        '''
         attrs = self.__pid_attrs()
         return attrs.get('threads')
 
     def rBytes(self):
+        '''
+        Class method: returns <type 'int'> of pids total read bytes since 
+        startup.
+        '''
         read = self._ntlnk_exchange()
         return read[0]
 
     def wBytes(self):
+        '''
+        Class method: returns <type 'int'> of the pids total write bytes since 
+        startup.
+        '''
         write = self._ntlnk_exchange()
         return write[1]
 
     def has_read(self):
+        '''
+        Class method: returns <type 'bool'> for pid's reads since last call.
+        '''
         current_r = self.rBytes()
         if current_r > self._last_r:
             self._last_r = current_r
@@ -80,6 +116,9 @@ class Profile(__NetLinkConn):
         return False
 
     def has_write(self):
+        '''
+        Class method: returns <type 'bool'> for the pids writes since last call.
+        '''
         current_w = self.wBytes()
         if current_w > self._last_w:
             self._last_w = current_w
@@ -87,14 +126,26 @@ class Profile(__NetLinkConn):
         return False
 
     def allread(self):
+        '''
+        Class method: returns <type 'int'> for pids total reads since object
+         creation.
+        '''
         current_r = self.rBytes()
         return current_r - self.__rst_state
 
     def allwrite(self):
+        '''
+        Class method: returns <type 'int'> for the pids total writes since
+        object creation.
+        '''
         current_w = self.wBytes()
         return current_w - self.__wst_state
 
     def __pid_attrs(self):
+        '''
+        Private class method: (not meant to be called directly) populates a
+        <type 'dict'> with pids attributes.
+        '''
         try:
             with open('/proc/%s/status' % self.pid) as f:
                 raw_attrs = f.readlines()
