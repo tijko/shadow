4117bb3c36f101c466154429e751296ca3edf6ce
Tim Konick <konick781@gmail.com>
2014-03-15 18:32:18 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+
+setup( 
+    name = 'shadow',
+    version = '0.0.1',
+    author='Tim Konick',
+    author_email='konick781@gmail.com',
+    url='',
+    description='Provides auxillary data about processes',
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
+    packages=['shadow'],
+    ext_modules = [Extension('limit', sources=['shadow/limit.c'])]
+)
