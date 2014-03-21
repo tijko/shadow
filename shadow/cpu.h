6a100e50c229af0583165a7c849cbe5d6bd89912
Tim Konick <konick781@gmail.com>
2014-03-20 20:04:31 -0400

    edit header for "isoproc"

--- shadow/cpu.h
+++ shadow/cpu.h
@@ -1,11 +1,15 @@
-/* 
- * Macro to set the correct system call number for 
- * getcpu, as the linux api doesn't provide a wrapper
- * for the call.
- */
+/* set getcpu syscall based on architecture */
 
 #ifdef __x86_64__
 #define GETCPU 309
 #elif __i386__
 #define GETCPU 318
 #endif
+
+#include <unistd.h>
+
+#define NPROC sysconf(_SC_NPROCESSORS_ONLN)
+
+int procek(char *dirname);
+
+int aotcpu(int isopid, size_t aotsize, cpu_set_t aotpid);
