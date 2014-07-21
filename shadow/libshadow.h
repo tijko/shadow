22ccd961b14bc7c874e3d1819d8de527cf70a8c7
Tim Konick <konick781@gmail.com>
2014-07-21 11:55:11 -0400

    place all includes inside header

--- shadow/libshadow.h
+++ shadow/libshadow.h
@@ -1,5 +1,11 @@
-
+#include <sched.h>
+#include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/resource.h>
+
 
 #define NPROC sysconf(_SC_NPROCESSORS_ONLN)
 
