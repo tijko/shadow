5c08263b47a7e47ad66bc00576ae42ae85bfae9b
Tim Konick <konick781@gmail.com>
2014-09-17 15:13:04 -0400

    add path of taskstats to packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from setuptools import setup, Extension
+    from setuptools import setup, Extension, find_packages
 except ImportError:
     from distutils.core import setup, Extension
 
@@ -16,6 +16,6 @@ setup(
     description='Provides auxillary data on processes',
     long_description=open('README.md').read(),
     license=open('LICENSE').read(),
-    packages=['shadow'],
+    packages=['shadow', 'shadow.taskstats'],
     ext_modules=[Extension('libshadow', sources=['shadow/libshadow/libshadow.c'])]
 )
