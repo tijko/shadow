d3bcab682d4adfbb58681ad4d4c9602393548ff6
Tim Konick <konick781@gmail.com>
2015-09-03 14:02:10 -0400

    Adjust declaration/initializations

--- shadow/libshadow/libshadow.c
+++ shadow/libshadow/libshadow.c
@@ -43,26 +43,26 @@ static PyObject *libshadow_maxlimit(PyObject *self, PyObject *args)
 
 static PyObject *libshadow_isoproc(PyObject *self, PyObject *args)
 {
-    int pid, ret, i;
     cpu_set_t isopid, aotpid;
-    size_t isosize, aotsize;
+
     CPU_ZERO(&isopid);
     CPU_ZERO(&aotpid);
-    isosize = CPU_ALLOC_SIZE(1);
-    aotsize = CPU_ALLOC_SIZE(NPROC - 1);
 
-    if (!PyArg_ParseTuple(args, "i", &pid)) 
-        return NULL;
+    size_t isosize = CPU_ALLOC_SIZE(1);
+    size_t aotsize = CPU_ALLOC_SIZE(NPROC - 1);
 
-    if  (NPROC <= 1)
+    int pid;
+    if (!PyArg_ParseTuple(args, "i", &pid) || NPROC <= 1) 
         return NULL;
 
     CPU_SET(0, &isopid);
-    ret = sched_setaffinity(pid, isosize, &isopid);
+
+    int ret = sched_setaffinity(pid, isosize, &isopid);
 
     if (ret < 0) 
         PyErr_SetString(ShadowErr, strerror(errno));
 
+    int i;
     for (i=1; i < NPROC; i++) 
         CPU_SET(i, &aotpid);
 
@@ -73,24 +73,26 @@ static PyObject *libshadow_isoproc(PyObject *self, PyObject *args)
 int procek(char *dirname)
 {
     char *procptr;
+
     strtod(dirname, &procptr);
+
     return *procptr == '\0';
 }
 
 int aotcpu(int isopid, size_t aotsize, cpu_set_t aotpid)
 {
-    int proc, ret;
-    char *base;
-    DIR *dir;
     struct dirent *cdir;
-    base = "/proc/";
-    dir = opendir(base);
+
+    char *base = "/proc/";
+    DIR *dir = opendir(base);
+
+    int proc;
 
     while ((cdir = readdir(dir))) {
         if (cdir->d_type == DT_DIR && procek(cdir->d_name)) {
             proc = strtol(cdir->d_name, NULL, 10);
             if (proc != isopid) 
-                ret = sched_setaffinity(proc, aotsize, &aotpid);
+                sched_setaffinity(proc, aotsize, &aotpid);
         }
     }
  
